5e81241892a087dbf428cc7f3804988c53bdfc04
Marta Lukiv <125505915+martalukiv@users.noreply.github.com>
2023-07-05 17:51:12 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+let number = prompt (`Write a number from 1 to 12`);
+
+if (number==1) {
+    console.log (`January`);   
+}
+
+
+if (number==2) {
+    console.log (`February`);
+}
+
+if (number==3) {
+    console.log (`March`);
+}
+
+if (number==4) {
+    console.log (`April`);
+}
+
+if (number==5) {
+    console.log (`May`);
+}
+
+if (number==6) {
+    console.log (`June`);
+}
+
+if (number==7) {
+    console.log (`July`);
+}
+
+if (number==8) {
+    console.log (`August`);
+}
+
+if (number==9) {
+    console.log (`September`);
+}
+
+if (number==10) {
+    console.log (`October`);
+}
+if (number==11) {
+    console.log (`November`);
+}
+if (number==12) {
+    console.log (`December`);
+}
+else if (number==null) {
+    console.log (`Cancelled`);
+}
+
+
+
